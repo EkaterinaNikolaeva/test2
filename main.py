5684e6aaec567987c095d73f7ec2fb350c8f7a2d
EkaterinaNikolaeva <57720667+EkaterinaNikolaeva@users.noreply.github.com>
2020-04-28 22:34:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from data import db_session
 from flask import Flask, render_template, redirect, request
+from loginform import LoginForm
+from registerform import RegisterForm
+import sqlite3
+from PIL import Image
+import io
+import maps
+import requests
+import lxml.etree
+from data import users
+from flask_login import LoginManager, login_user, logout_user, login_required
 import os
-
 
 
 app = Flask(__name__)
